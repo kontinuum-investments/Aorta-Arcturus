d733be5816bcf10b0c684b8c52d2283ec13d427b
Kavindu Athaudha <kavindu@kih.com.sg>
2023-11-20 10:33:04 +1300

    AORTA-41 Create Arcturus Svelte UI Library

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,18 +1,49 @@
 <script>
-  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+    import { page } from '$app/stores';
+    import {
+        Navbar,
+        NavBrand,
+        NavLi,
+        NavUl,
+        NavHamburger,
+        Dropdown,
+        DropdownHeader,
+        DropdownItem,
+        DropdownDivider,
+        Avatar
+    } from 'flowbite-svelte';
+
+    $: activeUrl = $page.url.pathname;
 </script>
 
-<Navbar  >
-  <NavBrand href="/">
-    <img src="https://flowbite-svelte.com/images/flowbite-svelte-icon-logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
-  </NavBrand>
-  <NavHamburger  />
-  <NavUl >
-    <NavLi href="/">Home</NavLi>
-    <NavLi href="/about">About</NavLi>
-    <NavLi href="/docs/components/navbar">Navbar</NavLi>
-    <NavLi href="/pricing">Pricing</NavLi>
-    <NavLi href="/contact">Contact</NavLi>
-  </NavUl>
+<Navbar let:NavContainer color='primary'>
+    <NavContainer class='border px-5 py-2 rounded-lg bg-white dark:bg-gray-600'>
+        <NavBrand href='/'>
+            <img src='https://flowbite-svelte.com/images/flowbite-svelte-icon-logo.svg' class='mr-3 h-6 sm:h-9'
+                 alt='Flowbite Logo' />
+            <span class='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>Flowbite</span>
+        </NavBrand>
+        <div class='flex items-center md:order-2'>
+            <Avatar id='avatar-menu' src='https://flowbite-svelte.com/images/profile-picture-3.webp' />
+            <NavHamburger class1='w-full md:flex md:w-auto md:order-1' />
+        </div>
+        <Dropdown placement='bottom' triggeredBy='#avatar-menu'>
+            <DropdownHeader>
+                <span class='block text-sm'>Bonnie Green</span>
+                <span class='block truncate text-sm font-medium'>name@flowbite.com</span>
+            </DropdownHeader>
+            <!--<DropdownItem>Dashboard</DropdownItem>-->
+            <!--<DropdownItem>Settings</DropdownItem>-->
+            <!--<DropdownItem>Earnings</DropdownItem>-->
+            <!--<DropdownDivider />-->
+            <DropdownItem>Sign out</DropdownItem>
+        </Dropdown>
+        <NavUl {activeUrl}>
+            <NavLi href='/'>Home</NavLi>
+            <NavLi href='/about'>About</NavLi>
+            <NavLi href='/docs/components/navbar'>Navbar</NavLi>
+            <NavLi href='/pricing'>Pricing</NavLi>
+            <NavLi href='/contact'>Contact</NavLi>
+        </NavUl>
+    </NavContainer>
 </Navbar>
