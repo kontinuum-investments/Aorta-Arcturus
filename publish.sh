320ffc2ca87c8f45bc3ca3df303bcc7961f1c712
Kavindu Athaudha <kavindu@kih.com.sg>
2023-11-20 08:18:36 +1300

    AORTA-41 Create Arcturus Svelte UI Library

--- publish.sh
+++ publish.sh
@@ -6,13 +6,13 @@ then
     VALUE=aorta-arcturus-dev
     ARGUMENT=s/${KEY}/${VALUE}/g
 
-    sed -i {$ARGUMENT} package.json package-lock.json
+    sed -i {$ARGUMENT} package.json
 fi
 
 KEY=aortaarcturusversion
 VALUE=$(npm view aorta-arcturus-dev version)
 ARGUMENT=s/${KEY}/${VALUE}/g
 
-sed -i {$ARGUMENT} package.json package-lock.json
+sed -i {$ARGUMENT} package.json
 npm version patch -git-tag-version false
 npm publish
