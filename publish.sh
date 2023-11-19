928372bb5813eaa775e37708137ddfb3d2c17bba
Kavindu Athaudha <kavindu@kih.com.sg>
2023-11-20 07:53:29 +1300

    Setup GitHub Actions

--- publish.sh
+++ publish.sh
@@ -2,17 +2,17 @@
 
 if [ "$1" == "development" ]
 then
-    STRINGTOREPLACE=aorta-arcturus
-    REPLACEMENTSTRING=aorta-arcturus-dev
-    ARGUMENT=s/${STRINGTOREPLACE}/${REPLACEMENTSTRING}/g
+    KEY=aorta-arcturus
+    VALUE=aorta-arcturus-dev
+    ARGUMENT=s/${KEY}/${VALUE}/g
 
     sed -i {$ARGUMENT} package.json package-lock.json
 fi
 
-STRINGTOREPLACE=aortaarcturusversion
-REPLACEMENTSTRING=$(npm view aorta-arcturus version)
-ARGUMENT=s/${STRINGTOREPLACE}/${REPLACEMENTSTRING}/g
+KEY=aortaarcturusversion
+VALUE=$(npm view aorta-arcturus-dev version)
+ARGUMENT=s/${KEY}/${VALUE}/g
 
 sed -i {$ARGUMENT} package.json package-lock.json
-#npm version patch -git-tag-version false
+npm version patch -git-tag-version false
 npm publish
